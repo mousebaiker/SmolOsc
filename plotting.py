684b62d69c8d58a0711348854ca27ed9609b6432
U-DESKTOP-66Q324U\Алексей <mousebaiker@gmail.com>
2020-04-10 05:05:42 +0300

    Plot any arbitrary solution with optional analytical solution.

--- plotting.py
+++ plotting.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def plot_moments(ts, zeroth, first, second):
   fig, axes = plt.subplots(3, figsize=(15, 15), sharex=True)
+
   axes[0].plot(ts, zeroth)
   axes[0].set_title('Total concentration',fontsize=16)
   axes[1].plot(ts, first)
@@ -21,6 +22,19 @@ def plot_moments(ts, zeroth, first, second):
   axes[2].tick_params(axis='both', which='minor', labelsize=14)
   return fig, axes
 
+
+def plot_solution(k, solution, analytical=None):
+  fig, ax = plt.subplots(1, figsize=(15, 10))
+  ax.loglog(k, solution, label='Numerical solution')
+  if analytical is not None:
+    ax.loglog(k, analytical, label='Analytical')
+  ax.set_xlabel('Cluster size', fontsize=14)
+  ax.set_ylabel('Concentration', fontsize=14)
+  ax.legend(fontsize=14)
+  ax.grid()
+  ax.tick_params(axis='both', which='major', labelsize=14)
+  ax.tick_params(axis='both', which='minor', labelsize=14)
+  return fig, ax
 
 def create_solution_animation(ts, k, solutions, name, analytical=None, loglog=True):
   fig, ax = plt.subplots()
