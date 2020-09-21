96e79bdb09e82d63091d711c9e890422ae6dd412
U-DESKTOP-66Q324U\Алексей <mousebaiker@gmail.com>
2020-09-21 19:36:50 +0300

    Fix precision errors. Add a sanity check.
    Having monomers as a first argument made computations in integers which
    were then casted to floats, creating a lot of precision errors.
    Reversing the order fixes it.
    Additionally, asserts are made to make sure that the total rate is
    correct at each simulation step.

--- FDMCS/simulation.h
+++ FDMCS/simulation.h
@@ -3,25 +3,25 @@
 #include <vector>
 
 typedef struct {
-  int count;
-  int size;
-  float collision_rate;
+  long long count;
+  long long size;
+  double collision_rate;
 } Particle;
 
 typedef struct {
   int idx;
-  float remaining_rate;
+  double remaining_rate;
 } SearchResult;
 
 class Simulation {
  public:
   Simulation();
   Simulation(float fragmentation_rate, std::mt19937 rng);
-  void AddParticle(int size);
-  void AddMonomers(int num_monomers);
+  void AddParticle(long long size);
+  void AddMonomers(long long num_monomers);
   void DeleteParticle(int idx);
   void DeletePair(const std::pair<int, int>& idxs);
-  std::pair<int, int> FindPair(float rate);
+  std::pair<int, int> FindPair(double rate);
 
   void RunSimulationStep();
 
@@ -29,18 +29,20 @@ class Simulation {
 
  private:
   Particle& GetParticle(int idx);
-  float CollisionFunction(int first_size, int second_size);
+  float CollisionFunction(long long first_size, long long second_size);
 
-  void InsertParticle(int size, float rate);
+  void InsertParticle(long long size, double rate);
   void RemoveParticle(int idx);
 
-  SearchResult FindFirst(float rate);
+  SearchResult FindFirst(double rate);
   SearchResult FindSecond(SearchResult first);
+
+  double CountTotalRate();
 
   std::vector<Particle> small_particles;
   std::vector<Particle> big_particles;
   double total_rate;
-  int total_size;
+  long long total_size;
   std::mt19937 rng;
 
   float fragmentation_rate;
