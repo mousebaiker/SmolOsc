30810abcffdf422aa8a81eba3062daea298b8de9
mousebaiker <mousebaiker@gmail.com>
2020-09-21 22:12:01 +0300

    Add simple timing benchmark
    This is useful to get an idea if the change introduces too much overhead 
    and also to make stress testing easier.

---
+++ FDMCS/simulation_benchmark.cc
@@ -0,0 +1,53 @@
+#include "simulation.h"
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::nanoseconds;
+using std::chrono::microseconds;
+using std::chrono::duration_cast;
+
+namespace {
+
+void PrintParticles(const std::vector<Particle>& particles) {
+  for (const auto& particle : particles) {
+    std::cout << "Particle size: " << particle.size
+              << ", count: " << particle.count
+              << ", collision rate: " << particle.collision_rate << std::endl;
+  }
+}
+
+nanoseconds recordDuration(MultiplicationKernelSimulation& simulation, int num_iterations) {
+  auto start = high_resolution_clock::now();
+
+  for (int i = 0; i < num_iterations; i++) {
+    simulation.RunSimulationStep();
+  }
+
+  auto end = high_resolution_clock::now();
+  return end - start;
+}
+} // namespace
+
+int main(int argc, char const *argv[]) {
+
+  std::vector<int> initial_monomers{1000, 10000, 100'000, 1'000'000};
+
+  const int num_iterations = 10 * 1000 * 1000;
+
+  std::cout << std::setprecision(10);
+
+  std::cout << "Completing " << num_iterations << " for each experiment." << std::endl;
+  for (auto num_monomers : initial_monomers) {
+    const float fragmentation_rate = 1.0;
+    MultiplicationKernelSimulation simulation(fragmentation_rate, std::mt19937());
+    simulation.AddMonomers(num_monomers);
+    nanoseconds running_time = recordDuration(simulation, num_iterations);
+    std::cout << "  Running time for " << num_monomers << " monomers is " <<
+        duration_cast<microseconds>(running_time).count()  << " microseconds."<< std::endl;
+  }
+  return 0;
+}
